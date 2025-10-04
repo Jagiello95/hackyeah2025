d51c21d153747ab179d3743138a3f11919d117a3
Jagiello95 <and.jagiello@gmail.com>
2025-10-04 13:48:22 +0200

    add map controls

--- src/app/components/map/map.ts
+++ src/app/components/map/map.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Navigation } from '../../services/navigation';
+import { BehaviorSubject } from 'rxjs';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-map',
-  imports: [],
+  imports: [ReactiveFormsModule, MatSlideToggle, MatButtonModule],
   templateUrl: './map.html',
-  styleUrl: './map.scss'
+  styleUrl: './map.scss',
 })
 export class Map {
-
+  protected nav = inject(Navigation);
+  protected toggle$ = new BehaviorSubject(false);
 }
