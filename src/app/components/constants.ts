d0f519c7b0aaac9f3a0af90ab5ea71d82966852f
Jagiello95 <and.jagiello@gmail.com>
2025-10-04 13:42:37 +0200

    dashboard layout

---
+++ src/app/components/constants.ts
@@ -0,0 +1,2 @@
+export const initTimeout = 50;
+export const initBounceTimeout = 200;
--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { initBounceTimeout } from '../constants';
+import { Navigation } from '../../services/navigation';
+import { filter, take } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.scss'
+  styleUrl: './dashboard.scss',
 })
 export class Dashboard {
+  protected init = false;
+  protected navigation = inject(Navigation);
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.init = true;
+    }, initBounceTimeout);
 
+    this.navigation.redirect$
+      .pipe(
+        filter((url) => !window.location.href.includes(url)),
+        take(1)
+      )
+      .subscribe(() => {
+        this.init = false;
+      });
+  }
 }
